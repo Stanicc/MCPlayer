c8c9ed9e180572a8bc980cc9f1f3f4e809b64af7
Stanicc <stanicmucierres@gmail.com>
2021-10-09 21:44:45 -0300

    removed duplicated method

--- core/src/main/kotlin/stanic/mcplayer/core/ImageReader.kt
+++ core/src/main/kotlin/stanic/mcplayer/core/ImageReader.kt
@@ -1,10 +1,8 @@
 package stanic.mcplayer.core
 
-import java.awt.Image
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
-import kotlin.math.ceil
 
 object ImageReader {
 
@@ -14,26 +12,6 @@ object ImageReader {
         val selected = directory.listFiles { _, name -> name.startsWith("$position") }.firstOrNull() ?: return null
 
         return selected to ImageIO.read(selected)
-    }
-
-    fun resizeToMapScale(image: BufferedImage, scale: Int): BufferedImage {
-        val scaled = image.getScaledInstance(
-            ceil(image.width / 128 * scale.toDouble()).toInt(),
-            ceil(image.height / 128 * scale.toDouble()).toInt(),
-            Image.SCALE_SMOOTH
-        )
-
-        val resized = BufferedImage(
-            scaled.getWidth(null),
-            scaled.getHeight(null),
-            BufferedImage.TYPE_INT_ARGB
-        )
-
-        val graphics = resized.createGraphics()
-        graphics.drawImage(scaled, 0, 0, null)
-        graphics.dispose()
-
-        return resized
     }
 
 }
